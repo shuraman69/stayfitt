e6efa136397d42d1d2322b5b6ebd121be4c0709c
shuraman__69 <fateev@e-krit.ru>
2022-01-09 11:03:23 +0300

    challenges screen title fix

--- src/screens/ChallengesScreen/ChallengesScreen.tsx
+++ src/screens/ChallengesScreen/ChallengesScreen.tsx
@@ -1,14 +1,18 @@
 import {Animated, StyleSheet} from 'react-native'
 import {BLACK} from '../../helpers/colors'
-import {FC} from 'react'
+import {FC, useEffect, useLayoutEffect} from 'react'
 import {useFetchChallenges} from '../../hooks/ContentScreen/useFetchChallenges'
 import {ChallengesBlock} from './components/ChallengesBlock'
 import {empty} from '../../helpers/empty'
 import {Loader} from '../../components/Loader'
+import {useNavigation} from "@react-navigation/native";
 
 export const ChallengesScreen: FC = ({}) => {
  const {loading, activeChallenges, passedChallenges} = useFetchChallenges()
-
+ const navigation = useNavigation()
+ useLayoutEffect(() => {
+  navigation.setOptions({headerTitle: 'Мои челленджи'})
+ }, [])
  if (loading) return <Loader indicatorSize='large' bg={BLACK}/>
  return (
   <Animated.ScrollView style={styles.scrollContainer}>
